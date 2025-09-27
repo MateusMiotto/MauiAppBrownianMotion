b9cfa6fbe0d837b997c25031d82a0209f4c70997
Mateus Vinícius Miotto <mateusbpk@gmail.com>
2025-09-27 09:18:54 -0300

    FEAT: Responsividade ok

--- MauiAppBrownianMotion/Views/BrownianPage.xaml.cs
+++ MauiAppBrownianMotion/Views/BrownianPage.xaml.cs
@@ -6,21 +6,28 @@ namespace MauiAppBrownianMotion.Pages
     public partial class BrownianPage : ContentPage, IMauiView
     {
         readonly GbmDrawable drawable = new();
+        const double WideThreshold = 1100; // largura mínima para painel lateral
 
         public BrownianPage()
         {
             InitializeComponent();
             this.InjectViewModel();
 
-            var vm = (BindingContext as BrownianViewModel);
-
-            if (vm == null)
-                throw new InvalidOperationException("ViewModel não pode ser nulo");
+            var vm = (BindingContext as BrownianViewModel) ?? throw new InvalidOperationException("ViewModel não pode ser nulo");
 
             drawable.GetPaths = () => vm.Paths;
             Chart.Drawable = drawable;
-
             vm.RedrawRequested += () => Chart.Invalidate();
+
+            SizeChanged += BrownianPage_SizeChanged;
+        }
+
+        void BrownianPage_SizeChanged(object? sender, EventArgs e)
+        {
+#if WINDOWS
+            var state = Width >= WideThreshold ? "Wide" : "Narrow";
+            VisualStateManager.GoToState(LayoutGrid, state);
+#endif
         }
     }
 }
