d933517e0e4bfd30fdf3ad942390285f56758182
Mateus Vinícius Miotto <mateusbpk@gmail.com>
2025-09-27 08:24:56 -0300

    FEAT: Setup

--- MauiAppBrownianMotion/ViewModels/MainPageViewModel.cs
+++ MauiAppBrownianMotion/ViewModels/MainPageViewModel.cs
@@ -1,4 +1,8 @@
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Runtime.CompilerServices;
+using Windows.Devices.Bluetooth.Advertisement;
+
 
 namespace MauiAppBrownianMotion.ViewModels
 {
@@ -17,8 +21,40 @@ namespace MauiAppBrownianMotion.ViewModels
 
 
 
+
+
         }
 
 
+
+        [RelayCommand]
+        public async Task GerarSimulacao()
+        {
+
+
+        }
+
+
+
+
+
+        public static double[] GenerateBromnianMotion(double sigma, double mean, double initialPrice, int numDays)
+        {
+            Random rand = new();
+            double[] prices = new double[numDays];
+            prices[0] = initialPrice;
+
+            for (int i = 1; 1 < numDays; i++)
+            {
+                double u1 = 1.0 - rand.NextDouble();
+                double u2 = 1.0 - rand.NextDouble();
+                double z = Math.Sqrt(-2.0 * Math.Log(u1)) * Math.Cos(2.0 * Math.PI * u2);
+
+                double retornoDiario = mean + sigma * 7;
+
+                prices[i] = prices[i - 1] * Math.Exp(retornoDiario);
+            }
+            return prices;
+        }
     }
 }
