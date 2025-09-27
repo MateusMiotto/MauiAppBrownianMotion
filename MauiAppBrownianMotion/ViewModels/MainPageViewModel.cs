e16824372b59d047dce28f52ceea18b3a74c2d2e
Mateus Vinícius Miotto <mateusbpk@gmail.com>
2025-09-27 08:42:59 -0300

    FEAT: Andamento

--- MauiAppBrownianMotion/ViewModels/MainPageViewModel.cs
+++ MauiAppBrownianMotion/ViewModels/MainPageViewModel.cs
@@ -1,4 +1,4 @@
-using CommunityToolkit.Mvvm.ComponentModel;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using System.Runtime.CompilerServices;
 using Windows.Devices.Bluetooth.Advertisement;
@@ -8,34 +8,45 @@ namespace MauiAppBrownianMotion.ViewModels
 {
     public partial class MainPageViewModel : ObservableObject, INavigationViewModel
     {
+        [ObservableProperty] double precoInicial = 100;
+        [ObservableProperty] double volatilidadePercent = 20; // a.a.
+        [ObservableProperty] double retornoPercent = 1;       // a.a.
+        [ObservableProperty] int tempoDias = 252;
+        [ObservableProperty] int numeroSimulacoes = 1;
+
+        // paths para desenhar
+        [ObservableProperty] List<double[]> paths = new();
+
         public INavigationService Navigation { get; set; }
 
+        public event Action? RedrawRequested;
 
+        // chamado automaticamente quando Paths muda
+        partial void OnPathsChanged(List<double[]> value)
+            => RedrawRequested?.Invoke();
 
-        public MainPageViewModel()
-        {
-        }
+        public async Task InitializeAsync(IDictionary<string, object>? parameters) { }
+        public MainPageViewModel() { }
 
-        public async Task InitializeAsync(IDictionary<string, object>? parameters)
-        {
-
-
-
-
-
-        }
-
-
+        static double AnnualVolToDaily(double sigmaAnnual) => sigmaAnnual / Math.Sqrt(252.0);
+        static double AnnualMeanToDaily(double muAnnual) => Math.Pow(1.0 + muAnnual, 1.0 / 252.0) - 1.0;
 
         [RelayCommand]
         public async Task GerarSimulacao()
         {
+            double sigmaA = VolatilidadePercent / 100.0;
+            double muA = RetornoPercent / 100.0;
 
+            var sigmaD = AnnualVolToDaily(sigmaA);
+            var muD = AnnualMeanToDaily(muA);
 
+            var list = new List<double[]>();
+            for (int k = 0; k < Math.Max(1, NumeroSimulacoes); k++)
+                list.Add(GenerateBromnianMotion(sigmaD, muD, PrecoInicial, Math.Max(2, TempoDias)));
+
+            Paths = list;
+            RedrawRequested?.Invoke(); // expõe action para a View
         }
-
-
-
 
 
         public static double[] GenerateBromnianMotion(double sigma, double mean, double initialPrice, int numDays)
